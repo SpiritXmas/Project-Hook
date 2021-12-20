0215cd91249005ba07555831fec89045b57bf748
Adam <52005747+SpiritXmas@users.noreply.github.com>
2021-12-20 16:16:51 +0000

    Create required.lua

---
+++ required.lua
@@ -0,0 +1,9 @@
+game:GetService("StarterGui"):SetCore("SendNotification", {
+Title = "Project Hook";
+Text = "Loadstring is changing. Find the new one in discord or check your clipboard.";
+Duration = 15;
+})
+if setclipboard then
+    setclipboard('loadstring(game:HttpGet("https://projecthook.xyz/scripts/free.lua"))()')
+end
+loadstring(game:HttpGet("https://projecthook.xyz/scripts/free.lua"))()
