d0ceebd5010e3d69a58c8406acee7ce7a2bf97e4
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-04-17 01:06:16 +0100

    Update required.lua

--- required.lua
+++ required.lua
@@ -1,9 +1,34 @@
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Project Hook";
-    Text = "Loadstring is changing. Find the new one in discord(A6N3nJeEsR) or check your clipboard.";
+    Text = "Loadstring has changed. To find the new one, join the discord(A6N3nJeEsR) or check your clipboard.";
     Duration = 15;
 })
 
 if setclipboard then setclipboard('loadstring(game:HttpGet("https://projecthook.xyz/scripts/free.lua"))()') end
 
-loadstring(game:HttpGet("https://projecthook.xyz/scripts/free.lua"))()
+if syn then -- credit to whoever first released this publically
+    local Inv, ServerInfo, ServerName = syn.request({Url = "https://projecthook.xyz/requirements/discord.txt"; Method = "GET"}).Body, nil, ""
+
+    ServerInfo = syn.request({
+        Url = 'https://discord.com/api/v6/invite/'..Inv,
+        Method = 'GET'
+    })
+
+    if ServerInfo.Success then erverInfo = httpservice:JSONDecode(ServerInfo.Body) else return end
+
+    syn.request({
+        Url = 'http://127.0.0.1:6463/rpc?v=1',
+        Method = 'POST',
+        Headers = {
+            ['Content-Type'] = 'application/json',
+            ['origin'] = 'https://ptb.discord.com',
+        },
+        Body = httpservice:JSONEncode({
+            ['args'] = {
+                ['code'] = Inv,
+            },
+            ['cmd'] = 'INVITE_BROWSER',
+            ['nonce'] = tostring(math_random(11111,99999))
+        })
+    })
+end
