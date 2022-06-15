b6bbedf956408bd19f617de2cb45b4d9c6e8138b
Adam <52005747+SpiritXmas@users.noreply.github.com>
2022-06-15 14:30:43 +0100

    Update required.lua

--- required.lua
+++ required.lua
@@ -1,10 +1,26 @@
 game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Project Hook";
-    Text = "Link has changed. \n\nTo find the new one join\n discord.gg/phook\n";
-    Duration = 15;
+    Title = "Project Hook Notice";
+    Text = "Project hook free script has been moved. Find the new link in our discord.";
+    Duration = 120;
 })
 
-if setclipboard then setclipboard('discord.gg/phook') end
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = 'Уведомление о крючке проекта';
+    Text = "Скрипт Project hook free был перемещен. Найдите новую ссылку в нашем discord.";
+    Duration = 120;
+})
+
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "[DISCORD]";
+    Text = "discord.gg/projecth\ndiscord.gg/WvFQFH86dy";
+    Duration = 120;
+})
+
+
+if setclipboard then
+    setclipboard("discord.gg/WvFQFH86dy")
+end
 
 if syn then
     local Inv, ServerInfo, ServerName = syn.request({Url = "https://projecthook.xyz/requirements/discord.txt"; Method = "GET"}).Body, nil, ""
